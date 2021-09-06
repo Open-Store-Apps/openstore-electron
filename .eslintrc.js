99e00735c6bac8da2ce184ed6ef172cd2d16d5f7
Rafael Neri <rafepel@gmail.com>
2021-09-05 23:42:09 -0300

    First Commit

---
+++ .eslintrc.js
@@ -0,0 +1,90 @@
+const { resolve } = require('path');
+
+module.exports = {
+  parser: 'vue-eslint-parser',
+
+  parserOptions: {
+    ecmaVersion: 'es2021',
+    parser: '@typescript-eslint/parser',
+    tsconfigRootDir: __dirname,
+    project: resolve(__dirname, './tsconfig.json'),
+    extraFileExtensions: ['.vue'],
+    sourceType: 'module'
+  },
+
+  env: {
+    browser: true
+  },
+
+  // Rules order is important, please avoid shuffling them
+  extends: [
+    // Base ESLint recommended rules
+    'eslint:recommended',
+
+    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#usage
+    'plugin:@typescript-eslint/recommended',
+
+    // consider disabling this class of rules if linting takes too long
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+
+    // Uncomment any of the lines below to choose desired strictness, but leave only one uncommented!
+    // See https://eslint.vuejs.org/rules/#available-rules
+    // 'plugin:vue/vue3-essential', // Priority A: Essential (Error Prevention)
+    // 'plugin:vue/vue3-strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
+    'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
+
+    'airbnb-base'
+  ],
+
+  plugins: [
+    // required to apply rules which need type information
+    '@typescript-eslint',
+
+    // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-file
+    // required to lint *.vue files
+    'vue',
+
+  ],
+
+  globals: {
+    ga: 'readonly', // Google Analytics
+    cordova: 'readonly',
+    __statics: 'readonly',
+    __QUASAR_SSR__: 'readonly',
+    __QUASAR_SSR_SERVER__: 'readonly',
+    __QUASAR_SSR_CLIENT__: 'readonly',
+    __QUASAR_SSR_PWA__: 'readonly',
+    process: 'readonly',
+    Capacitor: 'readonly',
+    chrome: 'readonly'
+  },
+
+  // add your custom rules here
+  rules: {
+    'no-param-reassign': 'off',
+    'no-void': 'off',
+    'no-nested-ternary': 'off',
+    'max-classes-per-file': 'off',
+
+    'import/first': 'off',
+    'import/namespace': 'error',
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'prefer-promise-reject-errors': 'off',
+
+    // TypeScript
+    quotes: ['warn', 'single', { avoidEscape: true }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    // allow debugger during development only
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    '@typescript-eslint/no-unused-vars': ['warn', {'argsIgnorePattern': '_'}],
+    'no-unused-vars': ['warn', {'argsIgnorePattern': '_'}]
+  }
+}
---
+++ src/i18n/index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable quote-props */
+
+import enUS from './langs/en-US';
+import ptBR from './langs/pt-BR';
+import pt from './langs/pt';
+import es from './langs/es';
+
+export default {
+  'pt-BR': ptBR,
+  'pt': pt,
+  'en-US': enUS,
+  'es': es,
+};
